43a69e07d5b2834d9882c54225797075a7cff34d
Dizzy <739091458@qq.com>
2023-01-10 11:33:49 +0800

    Update listener notes

--- lib/src/listener/receive_push_listener.dart
+++ lib/src/listener/receive_push_listener.dart
@@ -2,8 +2,8 @@ import 'package:xxim_core_flutter/src/proto/core.pb.dart';
 
 /// 接收推送监听
 class ReceivePushListener {
-  final Function(MsgDataList msgDataList) onPushMsgDataList; // 消息列表推送
-  final Function(NoticeDataList noticeDataList) onPushNoticeDataList; // 通知列表推送
+  final Function(MsgDataList msgDataList) onPushMsgDataList; // 推送消息列表
+  final Function(NoticeDataList noticeDataList) onPushNoticeDataList; // 推送通知列表
 
   final Function(
     String reqId,
